fc71d890c0eeff170a8180be2bb8ddc576dd8c19
dloweieu2025-create <dlowe.ieu2025@student.ie.edu>
2026-04-23 11:45:13 +0200

    Fixed bugs in data_cleaner

--- src/data_cleaner.py
+++ src/data_cleaner.py
@@ -7,11 +7,11 @@ path = "/mnt/c/users/djlow/CP2_project/data/github_inventory_unfilltered.xlsx"
 
 def table_merger(df):
 
-    df_orders = df1["orders"] # Order ID
-    df_makesuse = df1["makes_use"] #Dish ID and ID
-    df_ingredients = df1["ingredients"] #ID
-    df_dishes = df1["dishes"] #Dish ID
-    df_consist_of = df1["consist_of"] #Dish ID and Order ID
+    df_orders = df["orders"] # Order ID
+    df_makesuse = df["makes_use"] #Dish ID and ID
+    df_ingredients = df["ingredients"] #ID
+    df_dishes = df["dishes"] #Dish ID
+    df_consist_of = df["consist_of"] #Dish ID and Order ID
 
     df = pd.merge(df_orders, df_consist_of, on="ORDERID")
     df = pd.merge(df, df_dishes, on="DISHID")
