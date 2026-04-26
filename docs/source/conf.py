660c955f2bcb6208a471a989c257ec2016657883
Luke1904 <lucap@DESKTOP-6BJF7IS.localdomain>
2026-04-26 11:19:03 +0200

    Finished sphinx doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,36 +1,33 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-project = 'CP2_project'
-copyright = '2026, Luca, Donovan & Sebastian'
-author = 'Luca, Donovan & Sebastian'
-release = '27.04.2026'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon'
-]
-
-templates_path = ['_templates']
-exclude_patterns = []
-
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
-
 import os
 import sys
 
 sys.path.insert(0, os.path.abspath('../../src'))
+
+project = 'Restaurant Inventory Analyzer'
+author = 'Luca, Donovan & Sebastian'
+release = '1.0.0'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.autosummary',
+]
+
+autosummary_generate = True
+autodoc_member_order = 'bysource'
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'sphinx_rtd_theme'
+
+html_theme_options = {
+    'navigation_depth': 4,
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+}
+
+html_static_path = ['_static']
+
+html_css_files = ['custom.css']
